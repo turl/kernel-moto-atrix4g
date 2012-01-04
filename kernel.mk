d41a1b1114fa66b6fca39e57bb7a80681ffe44cd
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-01-04 11:07:03 +0100

    kernel.mk: reset to default kernel dir (not required)

--- kernel.mk
+++ kernel.mk
@@ -1,20 +1,24 @@
+#####################################################################
+# helpers for kernel, not required by "make kernel" (nice to remember)
+#####################################################################
+
 all:
-	$(MAKE) -f kernel_atrix/Android.mk kernel kernel_modules_install ext_kernel_modules
+	$(MAKE) -f kernel/Android.mk kernel kernel_modules_install ext_kernel_modules
 
 zImage:
-	$(MAKE) -f kernel_atrix/Android.mk kernel
+	$(MAKE) -f kernel/Android.mk kernel
 
 modules:
-	$(MAKE) -f kernel_atrix/Android.mk kernel_modules_install
+	$(MAKE) -f kernel/Android.mk kernel_modules_install
 
 ext_modules:
-	$(MAKE) -f kernel_atrix/Android.mk ext_kernel_modules
+	$(MAKE) -f kernel/Android.mk ext_kernel_modules
 
 config:
-	$(MAKE) -f kernel_atrix/Android.mk kernel_config
+	$(MAKE) -f kernel/Android.mk kernel_config
 
 dir:
-	$(MAKE) -f kernel_atrix/Android.mk kernel_dir
+	$(MAKE) -f kernel/Android.mk kernel_dir
 
 clean:
-	$(MAKE) -f kernel_atrix/Android.mk kernel_clean ext_kernel_modules_clean
+	$(MAKE) -f kernel/Android.mk kernel_clean ext_kernel_modules_clean
